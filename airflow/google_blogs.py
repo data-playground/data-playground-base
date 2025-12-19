05011226e7e78b45a5fec370bf5995bc32bf2e51
pedromanodemoura <67607441+pedromanodemoura@users.noreply.github.com>
2025-12-19 08:42:18 -0500

    Making necessary changes to call functions inside venv

--- airflow/google_blogs.py
+++ airflow/google_blogs.py
@@ -11,43 +11,64 @@ from airflow.decorators import dag, task, task_group
 folder_to_add = "/home/main-server/Github/data-playground-base/google_blogs"
 
 # Path to Pyhton virtual environment
-PATH_TO_PYTHON_BINARY = f"{folder_to_add}/venv/bin/activate"
+PATH_TO_PYTHON_BINARY = f"{folder_to_add}/venv/bin/python"
 
 # Add folder to system path
 sys.path.append(os.path.abspath(folder_to_add))
 
-@dag(schedule=None, start_date=datetime(2025, 1, 1), catchup=False, default_args={"python": PATH_TO_PYTHON_BINARY})
+@dag(
+    schedule=None, 
+    start_date=datetime(2025, 1, 1), 
+    catchup=False, 
+    default_args={
+        "python": PATH_TO_PYTHON_BINARY, 
+        "env": {"PYTHONPATH": folder_to_add}
+    }
+)
+
 def google_blogs():
 
-    @task(task_id="proc_start")
+    @task.external_python(task_id="proc_start", python=PATH_TO_PYTHON_BINARY)
     def create_data():
         # Import process script
         import data_gathering
 
         # Return a simple dictionary (always serializable)
         return data_gathering.init()
+    
+    @task.external_python(task_id="get_google_keys", python=PATH_TO_PYTHON_BINARY)
+    def get_website_keys(init_data):
+        return [name for name in init_data['WEBSITES'].keys() 
+                if name.startswith("GOOGLE") and name != 'GOOGLE_DEVS_SITEMAP']
+
+    @task.external_python(task_id = "gather_individual_blog", map_index_template="{{ task.op_kwargs['name'] }}", python=PATH_TO_PYTHON_BINARY)
+    def use_data(init, name):
+        # Import process script
+        import data_gathering
+
+        url = init['WEBSITES'][name]
+
+        # Pass the data into your logic function
+        if name == "GOOGLE_WORKSPACE_BLOG":
+            data_added = data_gathering.workspace_data_get(init)
+        elif name in ["GOOGLE_APPS_UPDATES", "GOOGLE_TECHNOLOGY_BLOG"]:
+            data_added = data_gathering.fetch_and_parse_feed(init, url, name, enrich=True)
+        else:
+            data_added = data_gathering.fetch_and_parse_feed(init, url, name, enrich=False)
+
+        return data_added
 
     @task_group(group_id="google_data")
-    def gather_google_data(init):
-        for name, url in init['WEBSITES'].items():
-            if name.startswith("GOOGLE") and name not in ['GOOGLE_DEVS_SITEMAP']:
-                @task(task_id=f"gather_{name}")
-                def use_data(init, url, name):
-                    # Import process script
-                    import data_gathering
+    def gather_google_data(names_list, init_obj):
+        # DYNAMIC MAPPING: Creates one 'use_data' task for every name in site_names
+        use_data.partial(init=init_obj).expand(name=names_list)
 
-                    # Pass the data into your logic function
-                    if name == "GOOGLE_WORKSPACE_BLOG":
-                        data_added = data_gathering.workspace_data_get(init)
-                    elif name in ["GOOGLE_APPS_UPDATES", "GOOGLE_TECHNOLOGY_BLOG"]:
-                        data_added = data_gathering.fetch_and_parse_feed(init, url, name, enrich=True)
-                    else:
-                        data_added = data_gathering.fetch_and_parse_feed(init, url, name, enrich=False)
 
-                    return data_added
+    # Execution Flow
+    init_obj = create_data()
+    site_names = get_website_keys(init_obj)
+    
+    gather_google_data(site_names, init_obj)
 
-    # Flow: Data is passed via XCom automatically
-    blog_sites_init = create_data()
-    gather_google_data(blog_sites_init)
 
 google_blogs()
